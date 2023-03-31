232bf4bf53243365731cd3d06263f9096cdb3e1d
Levislela <126335604+Levislela@users.noreply.github.com>
2023-03-31 06:17:00 +0000

    LEVIS LE LAIT

--- config.js
+++ config.js
@@ -7,13 +7,14 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.Owner = ['0545752448']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['0545752448']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['0545752458']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.BotName = "Miku"
-global.packname = "Miku bot"
+global.packname = "LEVIS LE LAIT "
+
 global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.OwnerName = "Fl"TDH YOGIRI TAKATOU"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
